8af8ebaac718a2f9ad8f93461253936999a8e2c0
Paul1005 <mccarliep@gmail.com>
2019-08-04 12:35:38 -0700

    added input from user

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 import classes.BeliefRevision;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        String k = "A, B, C";
-        String phi = "!A && !B";
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        // Give user instructions
+        System.out.println("Please enter K, separate different statements with a comma and a space, press enter when complete");
+        String k = scanner.nextLine();
+        System.out.println("Please enter Phi, press enter when complete");
+        String phi = scanner.nextLine();
+        //String k = "A, B, C || A, !C";
+        //String phi = "!A";
         BeliefRevision beliefRevision = new BeliefRevision();
 
         System.out.println(beliefRevision.reviseBelief(k, phi));
